7b36fde3cd9d2cc1055aae6d071fe1711a28443c
trackingfox <sharifali1993@gmail.com>
2021-09-07 00:38:48 +0530

    Changed file Commit.

--- src/com/JPA/onlineExam/service/QuestionService.java
+++ src/com/JPA/onlineExam/service/QuestionService.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.JPA.onlineExam.model.Question;
-import com.JPA.onlineExam.model.QuestionTemp;
 import com.JPA.onlineExam.repository.QuestionRepository;
 import com.opencsv.bean.CsvToBeanBuilder;
 
@@ -27,26 +26,7 @@ public class QuestionService {
 		this.repository = repository;
 	}
 
-//	public List<Question> importQuestionFromCsv() throws IllegalStateException, FileNotFoundException {
-//
-//		String filename = "data/MCQDBSample2.csv";
-//
-//		ClassLoader classloader = getClass().getClassLoader();
-//		String filePath = classloader.getResource(filename).getPath();
-//		// System.out.println(filePath);
-//
-//		List<Question> QuestionList = new CsvToBeanBuilder(new FileReader(filePath)).withType(Question.class).build()
-//				.parse();
-//		return QuestionList;
-//	}
-//
-//	public void populateQuestion() throws IllegalStateException, FileNotFoundException {
-//		List<Question> questionList = importQuestionFromCsv();
-//		System.out.println(questionList.toString());
-//		questionList.forEach(x -> repository.save(x));
-//	}
-
-	public List<QuestionTemp> importQuestionTempFromCsv() throws IllegalStateException, FileNotFoundException {
+	public List<Question> importQuestionFromCsv() throws IllegalStateException, FileNotFoundException {
 
 		String filename = "data/MCQDBSample2.csv";
 
@@ -54,18 +34,40 @@ public class QuestionService {
 		String filePath = classloader.getResource(filename).getPath();
 		// System.out.println(filePath);
 
-		List<QuestionTemp> QuestionList = new CsvToBeanBuilder(new FileReader(filePath)).withType(QuestionTemp.class)
-				.build().parse();
+		List<Question> QuestionList = new CsvToBeanBuilder(new FileReader(filePath)).withType(Question.class).build()
+				.parse();
 		return QuestionList;
 	}
 
-	public void populateQuestionTemp() throws IllegalStateException, FileNotFoundException {
-		List<QuestionTemp> questionTempList = importQuestionTempFromCsv();
-		// System.out.println(questionList.toString());
-		for (QuestionTemp Q : questionTempList)
-
-			questionList.forEach(x -> repository.save(x));
+	public void populateQuestion() throws IllegalStateException, FileNotFoundException {
+		List<Question> questionList = importQuestionFromCsv();
+		System.out.println(questionList.toString());
+		questionList.forEach(x -> repository.save(x));
 	}
+
+//	public List<QuestionTemp> importQuestionTempFromCsv() throws IllegalStateException, FileNotFoundException {
+//
+//		String filename = "data/MCQDBSample2.csv";
+//
+//		ClassLoader classloader = getClass().getClassLoader();
+//		String filePath = classloader.getResource(filename).getPath();
+//		// System.out.println(filePath);
+//
+//		List<QuestionTemp> QuestionList = new CsvToBeanBuilder(new FileReader(filePath)).withType(QuestionTemp.class)
+//				.build().parse();
+//		return QuestionList;
+//	}
+//
+//	public void populateQuestionTemp() throws IllegalStateException, FileNotFoundException {
+//		List<QuestionTemp> questionTempList = importQuestionTempFromCsv();
+//		// System.out.println(questionList.toString());
+//		for (QuestionTemp Q : questionTempList) {
+//			
+//			Q
+//		}
+//
+//			questionList.forEach(x -> repository.save(x));
+//	}
 
 	public Set<Question> fetchQues() {
 		Set<Question> results = repository.fetchQuestions();
